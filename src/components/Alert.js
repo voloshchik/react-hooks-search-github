73601ba35244478134034a61cb08eaeda9ccb994
vladimir <lovkiy2012@gmail.com>
2020-10-02 23:03:32 +0300

    finish

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,20 +1,17 @@
-import React, { useContext,useState } from "react";
-import { AlertContext } from "../context/alert/alertContext";
+import React, { useContext, useState } from 'react';
+import { AlertContext } from '../context/alert/alertContext';
 
 const Alert = () => {
   const { alert, hide } = useContext(AlertContext);
-  
-  console.log('alert', alert)
+
+  console.log('alert', alert);
   if (!alert) return null;
 
   return (
-    <div
-      className={`alert alert-${alert.type || "secondary"} alert-dismissible `}
-      role="alert"
-    >
+    <div className={`alert alert-${alert.type || 'secondary'} alert-dismissible `} role='alert'>
       {alert.text}
-      <button type="button" className="close" aria-label="Close" onClick={hide}>
-        <span aria-hidden="true">&times;</span>
+      <button type='button' className='close' aria-label='Close' onClick={hide}>
+        <span aria-hidden='true'>&times;</span>
       </button>
     </div>
   );
